e3e9369f5d1023c72c4bbdead94ce2cd919d4706
Aspandey07 <meakp321@gmail.com>
2026-05-22 01:17:37 +0530

    Auto-added Live Render API Link

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.PROD 
+  ? 'https://trippilot-ai-app.onrender.com/api' 
+  : 'http://localhost:5000/api';
+
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // During production, this can be changed
+  baseURL: baseURL,
 });
 
 api.interceptors.request.use((config) => {
